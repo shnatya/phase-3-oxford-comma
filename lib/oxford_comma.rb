0fff187864d0d0136fcdc299dcfaf1d17f784118
Anastasia Shatokhina <shnatya@gmail.com>
2022-01-22 16:13:48 -0800

    commit tree b9ef28d12bb2296b7f346a1b13b05ea1ce9a77eb

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
+require "pry"
 def oxford_comma(array)
-
+    message = if array.length == 1
+                array.join
+            elsif array.length == 2
+                array.join(" and ")
+            else array.length >= 3
+                last_el = array.last
+                final_array = array.map.with_index do |word|
+                    if word == last_el
+                        "and " + word
+                    else 
+                        word + ", "
+                    end
+                end
+                final_array.join
+            end  
+    message
 end
